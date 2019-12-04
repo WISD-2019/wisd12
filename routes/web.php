1f82a73ec15522b06e5320589590ad8b0d2a5d19
M511-03\admin <moon9638150@gmail.com>
2019-12-04 17:04:09 +0800

    bb.b2

--- routes/web.php
+++ routes/web.php
@@ -22,3 +22,6 @@ Route::get('/home', 'HomeController@index')->name('home');
 Route::get('/bb/b1', function () {
     return view('bb.b1');
 });
+Route::get('/bb/b2', function () {
+    return view('bb.b2');
+});
