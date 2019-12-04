c91231b816a34388b118bf88e3f34f2b47f74352
3A432055 <david860120@gmail.com>
2019-12-04 17:00:38 +0800

    aa.a2

--- routes/web.php
+++ routes/web.php
@@ -18,3 +18,11 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+
+Route::get('/aa/a1', function(){
+    return view('aa.a1');
+});
+
+Route::get('/aa/a2', function(){
+    return view('aa.a2');
+});
